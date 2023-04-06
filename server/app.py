9d7d943fdf5113d00429010a8e4e01688608d035
ianstrom <iankstrom@gmail.com>
2023-04-06 15:51:10 -0400

    Fixing database population. Changes to server/app.py

--- server/app.py
+++ server/app.py
@@ -1,24 +1,21 @@
 
 
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db.init_app(app)
 
 migrate = Migrate(app, db)
 
-db.init_app(app)
 # server/app.py
 
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 import json
-
-app = Flask(__name__)
 
 # CORS(app)
 
@@ -50,10 +47,6 @@ def upload():
     transcript = openai.Audio.transcribe("whisper-1", file=audio_file)
 
     return transcript
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
 
 if __name__ == '__main__':
 
