ba242138a979e91d63c3f8695f5a0cb2f2b3b76e
Michelle <choi.michelle.12@gmail.com>
2023-04-05 22:57:29 -0400

    now if you add an audio file, the transcript will populate in console log

--- client/src/SummarizeAudio.js
+++ client/src/SummarizeAudio.js
@@ -1,194 +1,162 @@
-import React from "react";
-import { useState, useEffect } from 'react';
-import './App.css';
-
-import axios from "axios";
-
-import paste from './images/paste.svg'
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function SummarizeAudio() {
+  const [audioFile, setAudioFile] = useState(null);
 
-    const [prompt, setPrompt] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!audioFile) {
+      alert('Please select a file to upload.');
+      return;
+    }
 
-    const [response, setResponse] = useState("");
-    const [name, setName] = useState()
-    const [relationship, setRelationship] = useState()
-    const [company, setCompany] = useState()
+    // Create a new form data object
+    const formData = new FormData();
 
-    const [description, setDescription] = useState()
-    const [accomplishments, setAccomplishments] = useState()
-    const [skills, setSkills] = useState()
-    const [style, setStyle] = useState()
-    const [words, setWords] = useState(300)
+    // Append the audio file to the form data object
+    formData.append('audio', audioFile);
 
-    const [isLoading, setIsLoading] = useState(false)
-    const [hasCalledAPI, setHasCalledAPI] = useState(false)
-    const [url, setURL] = useState("TESTING")
-    const [descriptionPlaceholder, setDescriptionPlaceholder] = useState()
+    // Send the form data to the backend server
+    try {
+      const response = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-          
-        const data = {
-            url
-        };
-      
-        axios
-          .post('/submit', data)
-          .then((response) => {
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    };
-    
-    useEffect(() => {
-        if (prompt !== "") {
-            axios
-                .post("http://localhost:8080/chat", { prompt })
-                .then((res) => {
-                    setHasCalledAPI(true)
-                    setIsLoading(false)
-                    console.log(res)
-                    setResponse(prevRes => {
-                        let newRes = res.data;
-                        return newRes;
-                    });
-                })
-                .catch((err) => {
-                    console.error(err);
-                });
-        }
-    }, [prompt])
+      // Log the response from the server
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
+  const handleFileChange = (e) => {
+    setAudioFile(e.target.files[0]);
+  };
+
+  return (
+    <div>
+      <h1>Upload Audio</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-            <div className="formTitle">
-                <h1 className="headTitle">Is that what I said?</h1>
-                <h2 className="headSub"> Let's transcribe and summarize!</h2>
-            </div>
+          <label htmlFor="audio">Select an MP3 file:</label>
+          <input type="file" id="audio" name="audio" accept=".mp3" onChange={handleFileChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
 
-            <br />
-            <br />
-
-            <form className="inputForm" onSubmit={handleSubmit}>
-                <div className="questionBox">
-                    {/* <img src={paste} className="numberSize" /> */}
-                    <div className="questionBoxText">
-                        <label className="label">Upload the audio file here. TO DO: LIST SUPPORTED FILE TYPES!</label>
-                        <div className="submitLink">
-                            <input name="description" id="jobDescription" className="input" placeholder="Your audio file transcription will appear here. " ></input>
-                            <button onClick={(e) => { 
-                                e.preventDefault();
-                                const data = {
-                                    file: e.target.parentNode.childNodes[0].value
-                                };
-                                axios
-                                    .post('/submit', data)
-                                    .then((response) => {
-                                        console.log(response.data.data.file);
-                                    })
-                                    .catch((error) => {
-                                        console.log(error);
-                                    });
-                            }}>
-                            </button>
-
-                        </div>
+// export default UploadAudio;
 
 
-                        <div className="questionBox">
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+// import axios from "axios";
+// import paste from './images/paste.svg'
 
-                        <div className="questionBoxText">
+// function SummarizeAudio() {
 
-                            <textarea defaultValue={descriptionPlaceholder} onChange={e => setDescription(e.target.value)} name="description" className="textareaInput" rows="10" placeholder="Your Transcription will appear here. "></textarea>
-                        </div>
-                        </div>
+//     const [prompt, setPrompt] = useState("");
+//     const [response, setResponse] = useState("");
+//     const [description, setDescription] = useState();
+//     const [words, setWords] = useState(300);
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [hasCalledAPI, setHasCalledAPI] = useState(false);
+//     const [url, setURL] = useState("TESTING");
+//     const [descriptionPlaceholder, setDescriptionPlaceholder] = useState();
 
-                    </div>
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         const data = {
+//             url
+//         };
+//         axios
+//           .post('/submit', data)
+//           .then((response) => {
+//             console.log(response.data);
+//           })
+//           .catch((error) => {
+//             console.log(error);
+//           });
+//     };
+    
+//     useEffect(() => {
+//         if (prompt !== "") {
+//             axios
+//                 .post("http://localhost:8080/chat", { prompt })
+//                 .then((res) => {
+//                     setHasCalledAPI(true)
+//                     setIsLoading(false)
+//                     console.log(res)
+//                     setResponse(prevRes => {
+//                         let newRes = res.data;
+//                         return newRes;
+//                     });
+//                 })
+//                 .catch((err) => {
+//                     console.error(err);
+//                 });
+//         }
+//     }, [prompt])
 
-                </div>
+//     return (
+//         <div>
+//             <div className="formTitle">
+//                 <h1 className="headTitle">Is that what I said?</h1>
+//                 <h2 className="headSub"> Let's transcribe and summarize!</h2>
+//             </div>
 
-                <br />
+//             <br />
+//             <br />
 
+//             <form className="inputForm" onSubmit={handleSubmit}>
+//                 <div className="questionBox">
+//                     <div className="questionBoxText">
+//                         <label className="label">Upload the audio file here. TO DO: LIST SUPPORTED FILE TYPES!</label>
+//                         <div className="submitLink">
+//                             <input name="description" id="jobDescription" className="input" placeholder="Your audio file transcription will appear here. " ></input>
+//                             <button onClick={(e) => { 
+//                                 e.preventDefault();
+//                                 const data = {
+//                                     file: e.target.parentNode.childNodes[0].value
+//                                 };
+//                                 axios
+//                                     .post('/submit', data)
+//                                     .then((response) => {
+//                                         console.log(response.data['file']);
+//                                         // let file_to_upload = response.data['file']
 
+//                                     })
+//                                     .catch((error) => {
+//                                         console.log(error);
+//                                     });
+//                             }}>
+//                             </button>
+//                         </div>
+//                         <div className="questionBox">
+//                             <div className="questionBoxText">
+//                                 <textarea defaultValue={descriptionPlaceholder} onChange={e => setDescription(e.target.value)} name="description" className="textareaInput" rows="10" placeholder="Your Transcription will appear here. "></textarea>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </form >
 
-                {/* options */}
+//             <br />
+//             <br />
 
-                {/* <h1 style={{ textAlign: "center", color: "#F67062" }}>Options</h1>
+//             <div id="Loading">
+//                 {isLoading ? <h1>"Worked!"</h1> : null}
+//             </div>
 
-                <div className="writingStylesDiv" >
-                    <img src={select} className="numberSize" />
-                    <label className="subLabel">Writing Styles</label>
+//             {hasCalledAPI ? <h1>"DIDNT WORK!"</h1> : null}
 
+//         </div >
+//     )
 
-                    <br />
-
-                    <div style={{ display: "flex" }}>
-                        <div className="styleContainer" onChange={(e) => setStyle(e.target.id)}>
-                            <input type="radio" id="formal" name="writingStyle" ></input>
-                            <label htmlFor="formal">
-                                <h2>Formal</h2>
-                                <p>Professional tone.</p>
-                                <p>Often used for academic or job-related recommendations.</p>
-                            </label>
-                        </div>
-
-                        <br />
-
-                        <div className="styleContainer" onChange={(e) => setStyle(e.target.id)}>
-                            <input type="radio" id="narrative" className="radioButton" name="writingStyle"></input>
-                            <label htmlFor="narrative" >
-                                <h2>Narrative</h2>
-                                <p>Storytelling format, highlighting the person’s achievements and qualities through anecdotes and examples.</p>
-                                <p>Often used for personal recommendations.</p>
-                            </label>
-                        </div>
-                    </div>
-
-                    <br />
-
-                    <div style={{ display: "flex" }}>
-                        <div className="styleContainer" onChange={(e) => setStyle(e.target.id)}>
-                            <input type="radio" id="bulletPoint" className="radioButton" name="writingStyle"></input>
-                            <label htmlFor="bulletPoint" >
-                                <h2>Bullet Point</h2>
-                                <p>Structured with short, concise statements that highlight the person’s skills and achievements.</p>
-                                <p>Often used when the recipient requires a quick summary of the person’s qualifications.</p>
-                            </label>
-                        </div>
-
-                        <br />
-
-                        <div className="styleContainer" onChange={(e) => setStyle(e.target.id)}>
-                            <input type="radio" id="comparative" className="radioButton" name="writingStyle"></input>
-                            <label htmlFor="comparative" >
-                                <h2>Comparative</h2>
-                                <p>Compares the person to others in their field, highlighting their strengths and qualifications. </p>
-                                <p>Often used in academic or job-related recommendations.</p>
-                            </label>
-                        </div>
-
-                    </div> */}
-
-
-            </form >
-
-            <br />
-            <br />
-
-            <div id="Loading">
-                {isLoading ? <h1>"Worked!"</h1> : null}
-            </div>
-
-
-            {hasCalledAPI ? <h1>"DIDNT WORK!"</h1> : null}
-
-
-        </div >
-    )
-                }
 
 export default SummarizeAudio
--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-# server/app.py
-
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 
@@ -10,30 +9,33 @@ app = Flask(__name__)
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
     data = request.get_json()
-    print(data)
-    # Changed 'echo' to 'print'
-    # x = data.keys()
-    # console.lo
-    # x.value
-    return {data}
+    data_json = json.dumps(data)
+    file = data_json['file']
+
+    return data_json
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'audio' not in request.files:
+        return 'No audio file in request.', 400
+
+    audio_file = request.files['audio']
+    audio_file.save('./uploads/test.mp3')
+    # TODO: Call Whisper API with audio file and return response
+    import io
+    import openai
+    openai.api_key = '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'
+    with open("./uploads/test.mp3", "rb") as audio_file:
+        audio_data = audio_file.read()
+    audio_file = io.BytesIO(audio_data)
+    audio_file.name = "test.mp3"
+    transcript = openai.Audio.transcribe("whisper-1", file=audio_file)
+
+    return transcript
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
-
-
-        # do something with the data here
-
-    # import io
-    # import openai
-    # # changed to michellees key
-    # openai.api_key = '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'
-
-    # # need to adjust this logic so that it intakes the dynamically generated file based on user input
-    # with open("./recording.m4a", "rb") as audio_file:
-    #     audio_data = audio_file.read()
-    # audio_file = io.BytesIO(audio_data)
-    # audio_file.name = "recording.m4a"
-    # transcript = openai.Audio.transcribe("whisper-1", file=audio_file)
-    # print(transcript)
-
-    # message = f'Transcript: {data}'
